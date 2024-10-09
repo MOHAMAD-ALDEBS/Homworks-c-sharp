a575631c70e44c1da38ff6397cd025cb5f2239e6
Muhammed ALDEBS <mdaldebs7@gmail.com>
2024-10-09 14:04:36 +0300

    week 11 hw

---
+++ week11hw/week11hw/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ 1. Topla_Carp(int a, int b, ref/out int add, ref/out int mult)  
+isimli bir fonksiyon yazınız.Bu fonksiyon içerisinden 
+hesaplanan topla ve carp değerlerini ana fonksiyon içerisinden
+ ekrana yazdırınız.
+*/
+
+namespace week11hw
+{
+    internal class Program
+    {
+
+        public void topla_carp(int a, int b , out int add , out int mult)
+        {
+            add = a + b;
+            mult = a * b;
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            int a, b;
+            Console.WriteLine("Enret the value of a :");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enret the value of b :");
+            b = Convert.ToInt32(Console.ReadLine());
+            
+            Program p = new Program();
+            int add, mult;
+
+            p.topla_carp(a, b, out add, out mult);
+
+            Console.WriteLine("the sum of a and b is :" + add);
+            Console.WriteLine("the multi of a and b is :" + mult);
+        }
+    }
+}
---
+++ week11hw/week11hw/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("week11hw")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("week11hw")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("a989e473-7acb-4e9d-85f2-fe0154a084ac")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
