d41d23979dd596bdd37c3e4d9ea6921f382c6a55
Muhammed ALDEBS <mdaldebs7@gmail.com>
2024-10-10 14:59:07 +0300

    week 14 ek homework

---
+++ gettinİD/gettinİD/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+ * ÖDEV :PersoneIimizin TC kmik Numarasını alacağız ve şu kontrollerin yapılmasını istiyoruz.
+SET ŞARTLARI
+ı. TC KİMLİK NO 'su 11 karakter olmalı
+2- Tüm karakterleri sayisal karakter olmalı
+GET ŞARTI
+TC kimlik no•sunun ilk 5 karakterini alacağız hepsini değil. 
+*/
+
+namespace gettinİD
+{
+    class personal
+    {
+        private string id;
+        public string ID
+        {
+
+            get
+            {
+                if (id != null)
+                {
+                    return id.Substring(0, 5);
+                }
+                return null;
+            }
+
+            set
+            {
+                if ( value.Length !=11 )
+                {
+                    Console.WriteLine("the tc is not 11 characters !");
+                }
+                else if (!value.All(char.IsDigit))
+                {
+                    Console.WriteLine("all characters has to be digit !!");
+                }
+                else { id = value; }
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            personal p1 = new personal();
+
+            while (p1.ID == null)
+            {   
+                Console.WriteLine("Enter the TC number please : ");
+                String input = Console.ReadLine();
+                p1.ID = input;      
+            }
+            Console.WriteLine("the first 5 ch is :" + p1.ID);
+        }
+    }
+}
---
+++ gettinİD/gettinİD/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("gettinİD")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("gettinİD")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("67c91058-eadc-434f-bee5-39b81571914a")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
